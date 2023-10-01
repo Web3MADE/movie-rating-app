3c6389253ab7d916f1d8002eaac060b0e57c7a16
Hakeem <mwo248@yahoo.com>
2023-10-01 13:47:25 +0400

    separate logic

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,18 +1,4 @@
-// 1. fetch movies
-// 2. filter by highest ratings
-import { NextResponse } from "next/server";
-import { data } from "../data";
-import { IMovie } from "../models";
+import getMovies from "../services/movies.service";
 export async function GET() {
-  try {
-    const filteredData = data.movies.map(
-      (movie: IMovie) => (movie.rating = Math.floor(Math.random() * 5) + 1)
-    );
-
-    console.log("filteredData ", filteredData);
-
-    return NextResponse.json(filteredData);
-  } catch (error) {
-    console.log("error ", error);
-  }
+  await getMovies();
 }
---
+++ app/api/services/movies.service.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { data } from "../data";
+import { IMovie } from "../models";
+
+export default async function getMovies() {
+  try {
+    const filteredData = data.movies.map(
+      (movie: IMovie) => (movie.rating = Math.floor(Math.random() * 5) + 1)
+    );
+
+    console.log("filteredData ", filteredData);
+
+    return NextResponse.json(filteredData);
+  } catch (error) {
+    console.log("error ", error);
+  }
+}
