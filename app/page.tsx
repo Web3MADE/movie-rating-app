898457f9e049dc1bc9fef32c708cdddce67399ab
Hakeem <mwo248@yahoo.com>
2023-10-02 22:28:29 +0400

    rename

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Home from "./components/Home";
-export default function HomePage() {
+export default function page() {
   // TODO: server side render here to fetch initial data
   // handle filtering of data in HomeComponent
   return <Home />;
