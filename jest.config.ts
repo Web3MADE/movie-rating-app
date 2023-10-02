6a02add398979926e26cdcee012b7d1502eae840
Hakeem <mwo248@yahoo.com>
2023-10-02 11:12:55 +0400

    fixed test setup

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,17 @@ const config: Config = {
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  transform: {
+    // This is necessary because next.js forces { "jsx": "preserve" }
+    "^.+\\.[jt]sx?$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          jsx: "react-jsx",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
