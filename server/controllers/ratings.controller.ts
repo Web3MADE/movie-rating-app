11c10df38e0c8de69eaad716cdf72a65df98d265
Hakeem <mwo248@yahoo.com>
2023-10-03 11:00:33 +0400

    fix service logic

--- server/controllers/ratings.controller.ts
+++ server/controllers/ratings.controller.ts
@@ -1,16 +1,16 @@
 import { Body, Controller, Path, Put, Route } from "tsoa";
 import { rateMovie } from "../services/ratings.service";
 
-// TODO: fix 404 error
+// TODO: fix 400 error
 @Route("ratings")
 export class RatingsController extends Controller {
   @Put("{id}")
   public async rateMovie(
     @Path() id: string,
-    @Body() rating: string
+    @Body() body: { rating: string }
   ): Promise<void> {
     try {
-      await rateMovie(id, rating);
+      await rateMovie(id, body.rating);
     } catch (error) {
       this.setStatus(500);
       console.error("Caught error", error);
--- server/services/ratings.service.ts
+++ server/services/ratings.service.ts
@@ -4,6 +4,7 @@ export async function rateMovie(id: string, rating: string) {
   try {
     const { movieCollection } = await getDatabase();
     let movie = null;
+    console.log("movieCollection ", movieCollection);
     if (movieCollection) {
       movie = await movieCollection
         .findOne({
@@ -13,10 +14,14 @@ export async function rateMovie(id: string, rating: string) {
         })
         .exec();
       if (movie) {
-        movie.rating = rating;
-        await movieCollection.upsert(movie);
+        console.log("movie selected ", movie);
+        await movieCollection.upsert({
+          ...movie,
+          rating: rating,
+        });
       }
     }
+    console.log("movie ", movie);
     return movie;
   } catch (error) {
     console.log("error ", error);
