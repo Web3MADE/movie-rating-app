aafaed40b004ae90b81504a26e36a55bda8493f0
Hakeem <mwo248@yahoo.com>
2023-10-04 10:28:39 +0400

    redirect added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/movies",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     domains: ["images-na.ssl-images-amazon.com", "ia.media-imdb.com"],
   },
